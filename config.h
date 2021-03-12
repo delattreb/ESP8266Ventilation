f8a409b68631deb84cfb51e12698fb637770222e
Bruno DELATTRE <delattreb@gmail.com>
2021-03-12 19:06:20 +0100

    First commit

---
+++ config.h
@@ -0,0 +1,20 @@
+//DEBUG INFORMATION
+#define SERIALLOG
+#define INFO
+#define DEBUG
+#define WIFIDEBUG
+
+// Serial configuration
+#define SERIALBAUDS 115200
+//Device name
+#define DEVICE_NAME "Ventilation1"
+
+//ESP8266 configuration
+#define NETWORKNAME "ESPVentilation"
+#define ATTENPTING 1000
+
+//Save Data
+#pragma region SAVE_DATA
+#define EEPROM_SIZE 1
+#define EEPROM_PLACE_BRIGHT 0
+#pragma endregion 
